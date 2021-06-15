ab91705ed8ef0b171cf992ca0c46627f72a652dd
kittidet2541 <55133201+kittidet2541@users.noreply.github.com>
2021-06-15 22:34:06 +0700

    Add files via upload

---
+++ Exercise4_kittidet_k.py
@@ -0,0 +1,9 @@
+FE=75.50
+GB=88.60
+ITC=99.99
+CP=49.00
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ITC)
+print("Computer Programming :",CP)
