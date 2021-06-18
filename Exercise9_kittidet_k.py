6d3992e735896b7236d572fe144f670a68e4c1ed
kittidet2541 <ktdklongklaw@gmail.com>
2021-06-19 00:30:16 +0700

    homework
    login

---
+++ Exercise9_kittidet_k.py
@@ -0,0 +1,8 @@
+userName=input("username:")
+password=input("password:")
+while userName!="kittidet" or password!="klongklaw":
+    print("please login again")
+    userName = input("username:")
+    password = input("password:")
+else:
+    print("Welcome to exchange oatabs")
