2985460285612d1e922b9dc61cd9dea4d3d2da08
kittidet2541 <ktdklongklaw@gmail.com>
2021-06-22 20:38:01 +0700

    lecture function

---
+++ functionExample.py
@@ -0,0 +1,15 @@
+def plusNumber(x,y):
+    print(str(x),"+",str(y),"=",x+y)
+def minusNumber(x,y):
+    print(str(x), "-", str(y), "=" ,x - y)
+def multiNumber(x,y):
+    print(str(x), "*", str(y), "=", x * y)
+def divNumber(x,y):
+    print(str(x), "/", str(y), "=", x / y)
+x=int(input("x="))
+y=int(input("y="))
+plusNumber(x,y)
+minusNumber(x,y)
+multiNumber(x,y)
+divNumber(x,y)
+
